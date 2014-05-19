b0cbde780da2110296a04db327f517e90e57fe6f
lilianoliveira <lilianaparecida@gmail.com>
2014-05-18 23:04:19 -0300

    Tentativas de implementação do trabalho
    Tentativas de implementação do trabalho

--- testes.m
+++ testes.m
@@ -32,3 +32,46 @@ filtroPassaAlta = [0, -1, 0; -1, 5, -1; 0, -1, 0]
 %':' é até
 % mean - merdia
 %std - desvio padrao
+
+matriz = [1,2;3,4]
+
+%circshift(matriz, [0,-1])
+%ans = 2,1; 4,3
+
+%circshift(matriz, [0,1])
+%ans = 2,1; 4,3
+
+%circshift(matriz, [1,0])
+%ans = 3,4;1,2
+
+%circshift(matriz, [-1,0])
+%ans = 3,4;1,2
+
+%circshift(matriz, [1,1])
+%ans = 4,3;2,1
+
+%circshift(matriz, [0,0])
+%ans = 1,2;3,4
+
+%para retornar a posica
+%matriz(1,2)
+%matriz(2,1)
+
+%multiplicacao elemento por elemento
+%result = img .* filtroPassaBaixass
+
+
+x=1
+y=2
+teste = [1,1;1,1]
+a = filtroPassaBaixa(x,y)*teste(x+1,y-1)
+
+   
+    %for linha 0:3
+     %   for coluna 0:3
+    %       teste(linha,coluna) = img(linha+1,coluna-1)*filtroPassaBaixa(linha,coluna) 
+   %     end
+  %  end
+
+
+
