90f34baa0a74490cea72865f7305417532a5f0c7
Victor Hugo <victor.hos@gmail.com>
2014-06-15 15:21:48 -0300

    subiu!

--- FiltrosDigitais.m
+++ FiltrosDigitais.m
@@ -4,6 +4,9 @@
 img = imread('/Users/victor/Pictures/lenna.jpg');
 image(img);
 
+%criando uma linha
+x  = linspace(1,256,256);
+
 %aplicando o algoritmo gaussiano (aplicar ruido)
 figure(1)
 img_r = img(:,:,1);
@@ -18,13 +21,15 @@ figure(2);
 colormap(gray);
 image(img_r_n);
 
+
 %criando filtro passa baixa
 filtroPassaBaixa = [0.12, 0.12, 0.12 ; 0.12, 0.12, 0.12; 0.12, 0.12, 0.12]
 
 %criando filtro passa alta
 filtroPassaAlta = [0, -1, 0; -1, 5, -1; 0, -1, 0]
 
-%algoritimo de convolucao
+%------------------------------------------------------------------------
+%algoritimo de convolucao PASSA BAIXA
 
 for x = 2:254
     for y = 2:254
@@ -57,18 +62,55 @@ figure(3)
 colormap(gray);
 image(imagem)
 
-%criando uma linha
-x  = linspace(1,256,256);
-
 %pega a linha 128, de 1 até 256
 linha_ruido = img_r_n(128,1:256);
-figure(4);
+figure(5);
 plot(x,linha_ruido,'b-');
 mean(linha_ruido)
 std(linha_ruido)
 
+%------------------------------------------------------------------------
+%algoritimo de convolucao PASSA ALTA
+
+for x = 2:254
+    for y = 2:254
+        
+        a = filtroPassaAlta(1,1) * img(x-1,y-1)
+        b = filtroPassaAlta(1,2) * img(x-1,y)
+        c = filtroPassaAlta(1,3) * img(x-1,y+1)
+        d = filtroPassaAlta(2,1) * img(x,y-1)
+        e = filtroPassaAlta(2,2) * img(x,y)
+        f = filtroPassaAlta(2,3) * img(x, y+1)
+        g = filtroPassaAlta(3,1) * img(x+1,y-1)
+        h = filtroPassaAlta(3,2) * img(x+1,y)
+        i = filtroPassaAlta(3,3) * img(x+1,y+1)
+        
+        imagem(x, y) = a + b + c + d + e + f + g + h + i;
+        
+    end
+end
+
+for q = 1:256
+    imagem(1,q) = img(2, q)
+    imagem(q,1) = img(q, 2)
+    
+    imagem(256,q) = img(255, q);
+    imagem(q, 256) = img(q, 255);
+end
+
+%Mostrando a imagem com passa alta
+figure(4)
+colormap(gray);
+image(imagem)
+
+%pega a linha 128, de 1 até 256
 linha_passabaixa = imagem(128,1:256);
-figure(5);
+figure(6);
 plot(x,linha_passabaixa,'r-');
 mean(linha_passabaixa)
 std(linha_passabaixa)
+
+
+%------------------------------------------------------------------------
+
+
